f0d3048bc7a5b402732621f7b38bddae0c376f6f
SatriaHadiWiyana7 <satriahadiwiyana7@gmail.com>
2024-04-23 18:22:27 +0700

    Melakukan Commant Pembuatan Menu

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include "Game.h"
+#include "Quest.h"
+
+Game game;
+Quest quest;
+
+int main()
+{
+    cout << "Tampilan oke" << endl;
+}
---
+++ src/Quest.h
@@ -0,0 +1,11 @@
+#ifndef QUEST_h
+#define QUEST_h
+
+class Quest
+{
+    // Hanya 2 Menu
+    // Menu game
+    // Menu save
+};
+
+#endif
