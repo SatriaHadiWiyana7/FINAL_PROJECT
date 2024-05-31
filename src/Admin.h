8f2eb82c69e95b0b4b772e7362a415a0eca7566a
SatriaHadiWiyana7 <satriahadiwiyana7@gmail.com>
2024-05-31 07:03:23 +0700

    Menambahkan Fungsi pada Admin dan Menambahkan Command Pada Quest

--- src/Admin.h
+++ src/Admin.h
@@ -45,12 +45,179 @@ private:
 public:
     Admin() : root(nullptr), isAdminLoggedIn(false)
     {
-        LoadFromFile(); // Load data on object creation
+        LoadFromFile();
     }
 
     NodeAdmin *GetRoot() const
     { // Fungsi untuk access root node
         return root;
+    }
+
+private:
+    // Fungsi untuk melkukan Save pada Player
+    void SavePlayerToFile(string username, string password, int poin)
+    {
+        ofstream dataFile;
+        dataFile.open("players.dat", ios::app);
+        if (dataFile.is_open())
+        {
+            dataFile << username << "," << password << "," << poin << endl;
+            cout << "Data pemain berhasil disimpan!" << endl;
+        }
+        else
+        {
+            cout << "Data Tidak Tersimpan!" << endl;
+        }
+    }
+
+    // Fungsi menemukan Admin dengan mengembalikan semua nilai pada admin
+    NodeAdmin *FindNode(string username)
+    {
+        NodeAdmin *current = root;
+        while (current != nullptr)
+        {
+            if (username == current->GetUsername())
+            {
+                return current;
+            }
+            else if (username < current->GetUsername())
+            {
+                current = current->left;
+            }
+            else
+            {
+                current = current->right;
+            }
+        }
+
+        return nullptr; // return nullptr
+    }
+
+    // Fungsi Update Tree Pada Admin
+    void UpdateFile()
+    {
+        ofstream adminFile("admin.dat", ios::out | ios::trunc);
+        if (adminFile.is_open())
+        {
+            if (root != nullptr)
+            {
+                adminFile << root->GetUsername() << "," << root->GetPassword() << "," << root->GetPosition() << endl;
+            }
+            UpdateFileHelper(root->left, adminFile);
+            UpdateFileHelper(root->right, adminFile);
+        }
+        else
+        {
+            cout << "Error updating admin file." << endl;
+        }
+        adminFile.close();
+    }
+
+    // Fungsi tambahan untuk membantu di Fungsi UpdateFile
+    void UpdateFileHelper(NodeAdmin *node, ofstream &adminFile)
+    {
+        if (node == nullptr)
+        {
+            return;
+        }
+
+        UpdateFileHelper(node->left, adminFile);
+        adminFile << node->GetUsername() << "," << node->GetPassword() << "," << node->GetPosition() << endl;
+        UpdateFileHelper(node->right, adminFile);
+    }
+
+    NodeAdmin *GetInorderSuccessor(NodeAdmin *node)
+    {
+        NodeAdmin *current = node;
+        while (current != nullptr && current->left != nullptr)
+        {
+            current = current->left;
+        }
+        return current;
+    }
+
+    // Fungsi melakukan Save pada Admin.dat
+    void SaveToFile(string username, string password, int position)
+    {
+        ofstream adminFile;
+        // Mode append
+        adminFile.open("admin.dat", ios::app);
+        if (adminFile.is_open())
+        {
+            adminFile << username << "," << password << "," << position << endl;
+        }
+        else
+        {
+            cout << "Data Tidak Tersimpan!" << endl;
+        }
+        adminFile.close();
+    }
+
+    // Fungsi melakukan Load Data Admin
+    bool LoadFromFile()
+    {
+        // Mode membaca
+        ifstream adminFile("admin.dat");
+        if (!adminFile.is_open())
+        {
+            cout << "Data Eror" << endl;
+            return false;
+        }
+
+        string line;
+        while (getline(adminFile, line))
+        {
+            // Objek stringstream bernama ss dibuat untuk memproses baris data line
+            stringstream ss(line);
+            // Inisialisasi Variabel
+            string username, password, s_position;
+            int position;
+            getline(ss, username, ',');
+            getline(ss, password, ',');
+            getline(ss, s_position);
+            // Merubah String kedalam Int
+            position = stoi(s_position);
+
+            // Fungsi membuat admin baru lalu menambahkan dengan username. password dan position yang telah terload
+            NodeAdmin *newNode = new NodeAdmin(username, password, position);
+
+            // Kondisi memasukan Data baru kedalam tree
+            if (root == nullptr) // Jika pada root kosong
+            {
+                root = newNode;
+            }
+            else
+            {
+                // Memposisikan Current pada posisi root
+                NodeAdmin *current = root;
+                // Melakukan inisialisasi parent menjadi 0
+                NodeAdmin *parent = nullptr;
+
+                while (current != nullptr)
+                {
+                    // Inisialisai parent menjadi current
+                    parent = current;
+                    if (username < current->GetUsername())
+                    {
+                        current = current->left;
+                    }
+                    else
+                    {
+                        current = current->right;
+                    }
+                }
+                if (username < parent->GetUsername())
+                {
+                    parent->left = newNode;
+                }
+                else
+                {
+                    parent->right = newNode;
+                }
+            }
+        }
+        adminFile.close();
+        return true;
     }
 };
 #endif
--- src/Quest_Medium.h
+++ src/Quest_Medium.h
@@ -7,9 +7,9 @@
 #include <sstream>
 #include <thread>
 #include <chrono>
-#include <algorithm> // fungsi shuffle
+#include <algorithm>
 #include <fstream>
-#define max 10
+#define max 100
 
 #include "game.h"
 using namespace std;
@@ -17,162 +17,261 @@ using namespace std;
 class Quest
 {
 public:
-    struct Question
+  Game game;
+  fstream QuestFile;
+
+  struct Question
+  {
+    string Soal[max];
+    string Kunci[max];
+    int Benar[max];
+    int Salah[max];
+  } Questions;
+
+  struct QUEUE
+  {
+    int tail;
+    int head;
+
+    Question Questions[max];
+  } Antrian;
+
+  bool used[max];
+  const int JUMLAH_SOAL = 6;
+
+  // kondisi pengecekan apakah string jawaban berupa string kosong
+  bool JawabanKosong(string jawaban)
+  {
+    return jawaban.empty();
+  }
+  // Fungsi Untuk Melakukan Radom Soal
+  void ShuffleQuestions()
+  {
+    // Questions.soal merupakan Alamat Array Awal
+    // Questions.soal + Antrian.tail merupakan Alamat Terakir pada Array yang akan di acak
+    // random_shuffle merupakan fungsi standart yang berasar dari library <algorithm>
+    random_shuffle(Questions.Soal, Questions.Soal + Antrian.tail);
+  }
+
+  // Fungsi Utama menggunakan Paramater Node &playerNode
+  void PlayGame(Node &playerNode)
+  {
+    // Pada Awal Hapus Semua data lalu Load kembali data
+    ClearData();
+    LoadAllData();
+
+    system("cls");
+    string Jawaban;
+    int Total_Score = 0, Score_Salah = 0, Score_Benar = 0, Jawaban_Benar = 0, Jawaban_Salah = 0, Soal_TidakTerjawab = 0, Total_Soal = 0;
+
+    bool restart = true; // Start game by default
+
+    // tread merupakan mikro program yang bisa berjalan dibalik program utama
+    thread timerThread; // Thread untuk mengatur waktu per soal
+
+    while (restart)
     {
-        string Soal[max];
-        string Kunci[max];
-        int Benar[max];
-        int Salah[max];
-    } Questions;
+      ShuffleQuestions(); // Mengacak soal
 
-    struct QUEUE
-    {
-        int tail;
-        int head;
-
-        Question Questions[max];
-    } Antrian;
-
-    bool used[max];
-    // Batasan jumlah soal
-    const int JUMLAH_SOAL = 5;
-
-    void PlayGame(string username, Player &player)
-    {
-        system("cls");
-        char Jawaban[200];
-        int Score = 0, Jawaban_Benar = 0, Jawaban_Salah = 0, Total_Soal = 0;
-
-        // Variabel untuk menyimpan status memulai ulang
-        bool restart = true; // true nilai default
-
-        while (restart) // Looping selama pemain ingin bermain ulang
+      for (int i = 0; i < JUMLAH_SOAL; i++)
+      {
+        int index = i;
+        if (!used[index])
         {
-            // Mengacak soal dan hanya menampilkan 5
-            vector<int> indexSoal(5); // Mengubah ukuran vector menjadi 5
-            for (int i = 0; i < 5; i++)
+          system("cls");
+          cout << "Nama Pemain : " << playerNode.GetUsername() << endl;
+          cout << "==========================" << endl;
+          cout << "Soal " << i + 1 << endl;
+          cout << "Soal : " << Questions.Soal[index] << endl;
+          cout << "Poin Benar : " << Questions.Benar[index] << endl;
+          cout << "Poin Salah : " << "-" << Questions.Salah[index] << endl;
+
+          int waktuSisa = 5; // Set Default Time
+          thread timerThread([&waktuSisa]()
+                             {
+            while (waktuSisa > 0) {
+              this_thread::sleep_for(chrono::seconds(1)); // Chrono berguna untuk memanipulasi waktu 1 detik berfungsi untuk membatasi waktu
+              waktuSisa--;
+
+              if (waktuSisa == 0) {
+                cout << "\nWaktu habis!" << endl;
+              }
+            } });
+
+          // Kondisi ini berfungsi agar ketika soal keluar pertama kita dapat Menjawab Soal Tersebut
+          if (i == 0)
+          {
+            cin.ignore();
+          }
+
+          // Input Jawaban
+          cout << "Masukkan Jawaban Anda : ";
+          getline(cin, Jawaban);
+
+          // Kondisi Pengecekan
+          if (!JawabanKosong(Jawaban))
+          {
+            if (Jawaban == Questions.Kunci[index])
             {
-                indexSoal[i] = i;
+              Score_Benar += Questions.Benar[index];
+              Jawaban_Benar++;
+              Total_Soal++;
+              used[index] = true;
             }
-            random_shuffle(indexSoal.begin(), indexSoal.end());
-
-            for (int i = 0; i < 5; i++)
+            else if (Jawaban != Questions.Kunci[index])
             {
-                int index = indexSoal[i]; // index merupakan soal setelah diacak pada fungsi diatas
-                if (!used[index])         // Periksa apakah soal belum pernah keluar
-                {
-                    system("cls");
-                    cout << "Soal " << i + 1 << endl;
-                    cout << Questions.Soal[index] << endl;
-                    cout << "Poin Benar : " << Questions.Benar[index] << endl;
-                    cout << "Poin Salah : " << "-" << Questions.Salah[index] << endl;
-                    cout << "Masukkan Jawaban Anda : ";
-                    cin >> Jawaban;
-
-                    if (Jawaban == Questions.Kunci[index])
-                    {
-                        Score += Questions.Benar[index];
-                        Jawaban_Benar++;
-                        Total_Soal++;
-                        used[index] = true; // Soal sudah keluar
-                    }
-                    else if (Jawaban != Questions.Kunci[index])
-                    {
-                        Score -= Questions.Salah[index];
-                        Jawaban_Salah++;
-                        Total_Soal++;
-                        used[index] = true; // Soal sudah Keluar
-                    }
-                    else
-                    {
-                        cout << "Skip Soal!" << endl;
-                    }
-                }
+              Score_Salah += Questions.Salah[index];
+              Jawaban_Salah++;
+              Total_Soal++;
+              used[index] = true;
             }
-
-            system("cls");
-            cout << "**Hasil Permainan**" << endl;
-            cout << "Nama Pemain : " << username << endl;
-            cout << "Skor : " << Score << endl;
-            cout << "Jawaban Benar : " << Jawaban_Benar << endl;
-            cout << "Jawaban Salah : " << Jawaban_Salah << endl;
-            cout << "Total Soal : " << Total_Soal << endl;
-
-            cout << "\nIngin bermain ulang? (y/n): ";
-            char pilihan;
-            cin >> pilihan;
-
-            if (pilihan != 'y' && pilihan != 'n')
-            {
-                cout << "Pilihan tidak valid. Masukkan y/n." << endl;
-                continue;
-            }
-
-            if (pilihan == 'n') // Berhenti bermain
-            {
-                restart = false;
-            }
-            else // Memulai ulang game
-            {
-                // Reset
-                for (int i = 0; i < Antrian.tail; i++)
-                {
-                    used[i] = false;
-                }
-                Score = 0;
-                Jawaban_Benar = 0;
-                Jawaban_Salah = 0;
-                Total_Soal = 0;
-            }
+          }
+          else
+          {
+            Soal_TidakTerjawab++;
+            Total_Soal++;
+            used[index] = true;
+          }
+          waktuSisa = 1;
+          timerThread.join();
         }
+      }
 
-        // Menambahkan skor ke player
-        player.poin += Score;
+      // Menampilkan Hasil Score
+      system("cls");
+      Total_Score = Score_Benar - Score_Salah;
+      cout << "**Hasil Permainan**" << endl;
+      cout << "Nama Pemain : " << playerNode.GetUsername() << endl;
+      cout << "Skor : " << Total_Score << endl;
+      cout << "Jawaban Benar : " << Jawaban_Benar << " -> Poin :" << Score_Benar << endl;
+      cout << "Jawaban Salah : " << Jawaban_Salah << " -> Poin :-" << Score_Salah << endl;
+      cout << "Soal Tidak Terjawab : " << Soal_TidakTerjawab << endl;
+      cout << "Total Soal : " << Total_Soal << endl;
 
-        cout << "\nTerima kasih telah bermain!" << endl;
+      // Menambahkan skor ke player
+      if (Total_Score <= 0)
+      {
+        playerNode.poin += Total_Score;
+      }
+      else if (Total_Score >= 0)
+      {
+        playerNode.poin += Total_Score;
+      }
+
+      if (playerNode.poin <= 0)
+      {
+        playerNode.poin = 0;
+      }
+      // Data Player akan TerUpdate pada Fungsi yang terdapat pada game
+      game.UpdatePlayerToFile(playerNode.username, playerNode.password, playerNode.poin);
+      cout << "\nTerima kasih telah bermain!" << endl;
+
+      cout << "Ingin bermain ulang? (y/n): ";
+      char pilihan;
+      cin >> pilihan;
+
+      if (pilihan != 'y' && pilihan != 'n')
+      {
+        cout << "Pilihan tidak valid. Masukkan y/n." << endl;
+        continue;
+      }
+
+      if (pilihan == 'n') // Berhenti bermain
+      {
+        // Reset
+        for (int i = 0; i < Antrian.tail; i++)
+        {
+          used[i] = false;
+        }
+        Total_Score = 0;
+        Score_Benar = 0;
+        Score_Salah = 0;
+        Jawaban_Benar = 0;
+        Jawaban_Salah = 0;
+        Total_Soal = 0;
+
+        restart = false;
+      }
+      else // Memulai ulang game
+      {
+        // Reset
+        for (int i = 0; i < Antrian.tail; i++)
+        {
+          used[i] = false;
+        }
+        Total_Score = 0;
+        Score_Benar = 0;
+        Score_Salah = 0;
+        Jawaban_Benar = 0;
+        Jawaban_Salah = 0;
+        Total_Soal = 0;
+      }
+    }
+  }
+
+  void ClearData()
+  {
+    // Reset Antrian
+    Antrian.tail = 0;
+    Antrian.head = 0;
+
+    // Reset used
+    for (int i = 0; i < max; i++)
+    {
+      used[i] = false;
     }
 
-    void LoadAllData()
+    // Kosongkan data Questions
+    for (int i = 0; i < max; i++)
     {
-        ifstream File;
-        File.open("Questions_Medium.dat", ios::in);
-
-        if (File.is_open())
-        {
-            string line, soal, kunci, strBenar, strSalah;
-            int benar, salah;
-
-            while (getline(File, line)) // Membaca baris demi baris
-            {
-                stringstream ss(line); // Memisahkan data dalam baris dengan koma
-
-                getline(ss, soal, ',');
-                getline(ss, kunci, ',');
-                getline(ss, strBenar, ',');
-                getline(ss, strSalah, ',');
-                benar = stoi(strBenar);
-                salah = stoi(strSalah);
-
-                // Menyimpan data soal
-                Questions.Soal[Antrian.tail] = soal;
-                Questions.Kunci[Antrian.tail] = kunci;
-                Questions.Benar[Antrian.tail] = benar;
-                Questions.Salah[Antrian.tail] = salah;
-
-                // Menyimpan status soal
-                used[Antrian.tail] = false;
-
-                Antrian.tail++;
-            }
-
-            File.close();
-        }
-        else
-        {
-
-            cout << "Error: Gagal membuka file Questions.dat." << endl;
-        }
+      Questions.Soal[i] = "";
+      Questions.Kunci[i] = "";
+      Questions.Benar[i] = 0;
+      Questions.Salah[i] = 0;
     }
+  }
+
+  void LoadAllData()
+  {
+    ifstream File;
+    File.open("Questions.dat", ios::in);
+
+    if (File.is_open())
+    {
+      string line, soal, kunci, strBenar, strSalah;
+      int benar, salah;
+
+      while (getline(File, line))
+      {
+        // Objek stringstream bernama ss dibuat untuk memproses baris data line
+        stringstream ss(line);
+        getline(ss, soal, ',');
+        getline(ss, kunci, ',');
+        getline(ss, strBenar, ',');
+        getline(ss, strSalah, ',');
+        // Merubah dari string ke Int
+        benar = stoi(strBenar);
+        salah = stoi(strSalah);
+
+        // Store data
+        Questions.Soal[Antrian.tail] = soal;
+        Questions.Kunci[Antrian.tail] = kunci;
+        Questions.Benar[Antrian.tail] = benar;
+        Questions.Salah[Antrian.tail] = salah;
+
+        // Mark question as loaded
+        used[Antrian.tail] = false;
+
+        Antrian.tail++;
+      }
+
+      File.close();
+    }
+    else
+    {
+      cout << "Error: Gagal membuka file Questions.dat." << endl;
+    }
+  }
 };
 
 #endif
